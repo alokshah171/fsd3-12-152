f3acbfc8b62e58ef652d4551659fd3cb5b59ea47
ALOK KUMAR <iamalok069@gmail.com>
2026-09-18 14:35:36 +0530

    update

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,30 +1,44 @@
 import http from "http";
-import { getUsers } from "./users.js";
-
+import {getUsers} from "./users.js"
 const server = http.createServer((req, res) => {
-  if (req.url === "/api/users" && req.method === "GET") {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <html>
+        <head>
+          <title>My Node Server</title>
+        </head>
+        <body>
+          <h1>Hello from Node.js!</h1>
+          <p>My server is working successfully.</p>
+          <p>This page is being served from port 2000.</p>
+        </body>
+      </html>
+    `);
+  }
+  else if (req.url === "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
-  } 
-  
-  else if (req.url === "/api/users" && req.method === "POST") {
+  } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    })
+    req.on('end', () => {
+      const user = JSON.parse(body);
+      console.log(user);
+    })
     res.end(JSON.stringify({ msg: "add user" }));
-  } 
-  
-  else if (req.url === "/api/users/1" && req.method === "GET") {
+  } else if (req.url === "/api/users/2" && req.method === "GET")
     res.end(JSON.stringify({ msg: "single user with id 1" }));
-  } 
-  
-  else if (req.url === "/api/users/1" && req.method === "PUT") {
+  else if (req.url === "/api/users/1" && req.method == "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
-  } 
-  
-  else if (req.url === "/api/users/1" && req.method === "DELETE") {
+  } else if (req.url === "/api/users/1" && req.method == "DELETE") {
     res.end(JSON.stringify({ msg: "remove 1" }));
-  } 
-  else {
-    res.statusCode = 404;
+  } else {
+    // res.statusCode = 404;
     res.end();
   }
 });
-
-server.listen(3000, () => console.log("prg7 is running"));
+server.listen(2000, () => {
+  console.log("Server is running on port 2000");
+});
